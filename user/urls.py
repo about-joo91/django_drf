0466e696fd5c78e81f1667cd138036db1e037f09
about-joo91 <about.joo91@gmail.com>
2022-06-19 20:15:31 +0900

    개인적으로 연습한 회원가입과 삭제

--- user/urls.py
+++ user/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from user import views
 urlpatterns = [
     path('user',views.UserView.as_view()),
+    path('user_control',views.UserControlView.as_view())
 ]
--- user/views.py
+++ user/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from .models import UserModel
-
-from django.contrib.auth import login, authenticate, logout
+from .models import UserModel, UserProfile
+from django.contrib.auth import login, authenticate, logout,hashers
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from .serializers import UserSerializer
+
 
 # Create your views here.
 
@@ -25,3 +25,34 @@ class UserView(APIView):
     def delete(self, request):
         logout(request)
         return Response({'message' : '로그아웃 성공!'}, status=status.HTTP_200_OK)
+
+class UserControlView(APIView):
+    def post(self,request):
+        username = request.data.get('username','')
+        password = request.data.get('password','')
+        email = request.data.get('email','')
+        bio = request.data.get('bio','')
+        full_name = request.data.get('full_name','')
+        age = request.data.get('age','')
+        new_user = UserModel.objects.create(
+            username = username,
+            password = hashers.make_password(password),
+            email = email,
+            full_name = full_name
+        )
+        new_user.save()
+        user_profile = UserProfile.objects.create(
+            user = new_user,
+            bio=bio,
+            age = age
+        )
+        user_profile.save()
+        return Response({
+            "message" : "회원가입 완료"
+        })
+    def delete(self, request):
+        cur_user = request.user
+        cur_user.delete()
+        return Response({
+            "message" : "삭제완료!"
+        })
